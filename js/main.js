763b3bedfa1171dbcf07614e6550501cf18c6733
T125059 <T125059@ed.sus.ac.jp>
2025-08-04 04:36:52 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,19 +4,19 @@ const lists = [
   {
     name: '諏訪湖',
     img: 'img/suwako.jpg', 
-    desc: '長野県最大の湖。湖畔には温泉や美術館が点在し、四季折々の美しい景色が楽しめます。',
+    desc: '長野県最大の湖です。湖畔には温泉や美術館が点在し、四季折々の美しい景色が楽しめます。',
     url: 'https://www.suwakanko.jp/story/%E8%AB%8F%E8%A8%AA%E6%B9%96%E3%83%BB%E7%BE%8E%E8%A1%93%E9%A4%A8/' 
   },
   {
     name: '霧ヶ峰高原',
     img: 'img/kirigamine.jpg',
-    desc: '夏にはニッコウキスゲが咲き誇る広大な高原。ハイキングコースとしても人気です。',
+    desc: '夏にはニッコウキスゲが咲き誇る広大な高原です。ハイキングコースとしても人気です。',
     url: 'https://kirigamine.suwakanko.jp'
   },
   {
     name: '諏訪大社 上社本宮',
     img: 'img/suwa-taisha.jpg',
-    desc: '全国に一万以上ある諏訪神社の総本社。荘厳な雰囲気の中で歴史を感じることができます。',
+    desc: '全国に一万以上ある諏訪神社の総本社です。荘厳な雰囲気の中で歴史を感じることができます。',
     url: 'https://suwataisha.or.jp'
   }
 ];
@@ -38,5 +38,6 @@ for (let i = 0; i < lists.length; i++) {
   
   menu.insertAdjacentHTML('beforeend', content);
 }
+
 
 console.log('おすすめ観光スポットの表示処理が完了しました。');
